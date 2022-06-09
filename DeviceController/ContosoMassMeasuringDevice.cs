0a0b519cd93f81eda573ea03a813ab7649442840
Илья Абрамёнок <i.abramenok@slata.com>
2022-06-09 19:24:32 +0800

    Добавьте файлы проекта.

---
+++ DeviceController/ContosoMassMeasuringDevice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DeviceControl;
+
+namespace Contoso.MeasuringDevices
+{
+    class MassMeasuringDevice : IControllableDevice
+    {
+        Random random;
+
+        public MassMeasuringDevice()
+        {
+            random = new Random();
+        }
+
+        public void StartDevice()
+        {
+            // Start the device.
+        }
+
+        public void StopDevice()
+        {
+            // Stop the device.
+        }
+
+        public int GetLatestMeasure()
+        {
+            return random.Next(1390);
+        }
+    }
+}
